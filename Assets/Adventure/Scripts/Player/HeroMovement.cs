b6124daaddebf759335f7be679b88502af9b1832
Artemiy <aptem64@yandex.ru>
2022-07-14 18:59:09 +0300

    Final

---
+++ Assets/Adventure/Scripts/Player/HeroMovement.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Adventure 
+{
+    public class HeroMovement : MonoBehaviour
+    {
+        private Animator animator;
+        private Rigidbody rigidbody;
+        private Vector3 direction;
+        private float vert;
+        private float hor;
+
+        private string horizontal = "Horizontal";
+        private string vertical = "Vertical";
+        private string jump = "Jump";
+
+        [SerializeField] private float speed = 4f;
+        [SerializeField] private float rotationSpeed = 15f;
+
+        private void Start()
+        {
+            rigidbody = GetComponent<Rigidbody>();
+            animator = GetComponent<Animator>();
+        }
+
+
+        // Update is called once per frame
+        private void Update()
+        {
+            hor = Input.GetAxis(horizontal);
+            vert = Input.GetAxis(vertical);
+
+            
+            direction = new Vector3(hor, 0, vert);
+            rigidbody.velocity = Vector3.ClampMagnitude(direction, 1) * speed;
+
+            if (direction.magnitude > Mathf.Abs(0.05f))
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(direction), rotationSpeed * Time.deltaTime);
+
+
+        }
+    }
+}
