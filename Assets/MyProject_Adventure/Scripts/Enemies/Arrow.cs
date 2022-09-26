be51ddff105e09dfc9fa101f3b60b9d15a5ccb13
Artemiy <aptem64@yandex.ru>
2022-09-26 15:45:34 +0300

    HomeWork_3_correction #3
    Final rework

--- Assets/MyProject_Adventure/Scripts/Enemies/Arrow.cs
+++ Assets/MyProject_Adventure/Scripts/Enemies/Arrow.cs
@@ -1,3 +1,4 @@
+using Adventure.Player;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,20 +7,37 @@ namespace Adventure.Enemies
 {
     public class Arrow : MonoBehaviour
     {
-        [SerializeField] private float speed = 5;
-
-
+        private float _speed = 100f;
+        private int _damage = 10;
+        private float timeDestroy = 3f;
+        private Rigidbody _rb;
 
         void Start()
         {
-            
+            _rb = GetComponent<Rigidbody>();
+            _rb.AddForce(Vector3.left * _speed * Time.deltaTime, ForceMode.Impulse); //ѕридание стреле силу импульса 
+            Invoke("DestroyBullet", timeDestroy); //уничтожение стрелы через опр. врем€, если не попала в игрока
+        }
+        /// <summary>
+        /// ћетод на получение урона, аналогичен урону от мины, только через коллизию
+        /// </summary>
+        /// <param name="collision"></param>
+        private void OnCollisionEnter(Collision collision)
+        {
+            if (collision.gameObject.CompareTag("Player"))
+            {
+                var player = collision.gameObject.GetComponent<PlayerHealth>();
+                player.Hurt(_damage);
+                Debug.Log("—трела нанесла: " + _damage);
+                Destroy(gameObject);
+            }
         }
 
-        
-        void Update()
+        void DestroyBullet()
         {
-            
+            Destroy(gameObject);
         }
+
     }
 }
 
--- Assets/MyProject_Adventure/Scripts/Enemies/ShootingEnemy.cs
+++ Assets/MyProject_Adventure/Scripts/Enemies/ShootingEnemy.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 
@@ -7,22 +8,55 @@ namespace Adventure.Enemies
 {
     public class ShootingEnemy : MonoBehaviour
     {
+        [SerializeField] private GameObject _target;
 
-        [SerializeField] private GameObject arrow;
+        [SerializeField] private float _rotateSpeed;
+
+        [SerializeField] private GameObject _arrow;
+        
+        // Поля простого таймера на стрельбу с шагом в 1 секунду
         [SerializeField] private Transform spawnPoint;
-        [SerializeField] private float spawnStep = 1f;
+        [SerializeField] private float spawnStep = 2f;
         private float nextSpawnTime;
-
 
         void Update()
         {
+            PlayerDetection();
+        }
+        /// <summary>
+        /// Метод обнаружения игрока, если игрок в пределах 10 метрах, 
+        /// то он делает фокус на нем и начинает стрелять
+        /// </summary>
+        private void PlayerDetection()
+        {
+            float _distance = Mathf.Sqrt((_target.transform.position - transform.position).sqrMagnitude);
+            if (_distance <= 10) // в этом поле условие, если игрок дальше 10м, то методы не вызываются
+            {
+                LookToPlayer();
+                Shoot();
+            }
+            Debug.Log("Дистанция до цели:" + _distance);
+        }
+        /// <summary>
+        /// Метод на поворот и фокус в сторону игрока
+        /// </summary>
+        private void LookToPlayer()
+        {
+            Vector3 _relativePos = _target.transform.position - transform.position;
+            Vector3 _newDir = Vector3.RotateTowards(transform.forward, _relativePos, _rotateSpeed * Time.deltaTime, 0);
+            transform.rotation = Quaternion.LookRotation(_newDir);
+        }
+        /// <summary>
+        /// Метод на "открытие" огня в сторону игрока
+        /// </summary>
+        private void Shoot()
+        {
             if (Time.time > nextSpawnTime)
             {
-                Instantiate(arrow, spawnPoint.position, Quaternion.identity);
+                Instantiate(_arrow, spawnPoint.transform.position, transform.rotation);
                 nextSpawnTime = Time.time + spawnStep;
-                
             }
-
         }
+
     }
 }
