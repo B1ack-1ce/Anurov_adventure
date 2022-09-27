df0e77375e5a542d5bca6acb7ba11053373f4bc2
Artemiy <aptem64@yandex.ru>
2022-09-27 19:40:32 +0300

    HomeWork_4_correction #1

--- Assets/MyProject_Adventure/Scripts/Player/PlayerMovement.cs
+++ Assets/MyProject_Adventure/Scripts/Player/PlayerMovement.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEditor.Rendering;
 using UnityEngine;
 using UnityEngine.EventSystems;
@@ -8,12 +9,14 @@ namespace Adventure.Player
 {
     public class PlayerMovement : MonoBehaviour
     {
-        [SerializeField] private float speed = 3f;
-        [SerializeField] private float rotationSpeed = 2f;
+        [SerializeField] private float _speed = 3f;
+        [SerializeField] private float _rotationSpeed = 2f;
 
-        private Vector3 direction;
+        private Vector3 _direction;
+        private Vector3 _rotationDir;
         private string hor = "Horizontal";
         private string ver = "Vertical";
+        private const string MouseX = "Mouse X";
         //private string jump = "Jump";
 
         private void Start()
@@ -29,16 +32,19 @@ namespace Adventure.Player
         
         private void Movement()
         {
-            direction.z = Input.GetAxis(hor);
-            direction.x = Input.GetAxis(ver);
+            _direction.z = Input.GetAxis(hor);
+            _direction.x = Input.GetAxis(ver);
 
-            var move = direction * speed * Time.deltaTime;
+          
+            var move = _direction * _speed * Time.deltaTime;
             transform.Translate(move);
         }
 
         private void Rotate()
         {
-            
+            transform.Rotate(_rotationDir);
+
+            _rotationDir.y = Input.GetAxis(MouseX) * _rotationSpeed * Time.deltaTime;
         }
 
     }
